bc380af41dae6e43729ed1843c379044c47340a7
Heloderma <sjosue43@gmail.com>
2018-12-26 23:25:03 -0600

    actualizacion del jquery

--- weatherApp/js/main.js
+++ weatherApp/js/main.js
@@ -1,1 +1,31 @@
-let key = '26b196d1c04ecf94ed7cef30ed360fdd';
+$(document).ready(function () {
+
+    $("#submitCity").click(function () {
+        return getWeather();
+    });
+
+});
+
+function getWeather() {
+    // const key = '26b196d1c04ecf94ed7cef30ed360fdd';
+    var city = $("#city").val();
+
+    if (city != "") {
+
+        $.ajax({
+
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&unit=metric" + "&APPID=26b196d1c04ecf94ed7cef30ed360fdd",
+            type: "GET",
+            datatype: "jsonp",
+            success: function (data) {
+                console.log(data);
+                $("#showWeather").html(data);
+            }
+
+        });
+
+    } else {
+        $("#error").html("<div>La ciudad no puede estar vacia</div>")
+    }
+
+}
