d1c5cb5e88d44161b8ce5b79ca43bc0bd66fb4ea
Heloderma <sjosue43@gmail.com>
2018-12-27 20:22:05 -0600

    dos proyectos mas

---
+++ REMSO/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        responsiveClass: true,
+        autoWidth: false,
+        navText: ['<i class="fas fa-arrow-circle-left" title="Anterior"></i>', '<i class="fas fa-arrow-circle-right" title="Siguiente"></i>'],
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            500: {
+                items: 2,
+                margin: 20,
+            },
+            800: {
+                items: 3,
+                margin: 20,
+            },
+            1000: {
+                items: 4,
+                margin: 20,
+            }
+        }
+    });
+});
