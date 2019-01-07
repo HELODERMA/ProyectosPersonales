b233a32bc004d56a00e840630c7cb2c53140e767
Heloderma <sjosue43@gmail.com>
2019-01-07 11:52:57 -0600

    Cambios finales de Remso y GDLwebCamp

--- DesarrolloWebCurso/GDLWebCamp/calendario.php
+++ DesarrolloWebCurso/GDLWebCamp/calendario.php
@@ -1,12 +1,12 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
-    <section class="section contenedor">
-        <h2>Calendario de eventos</h2>
+<section class="section contenedor">
+    <h2>Calendario de eventos</h2>
 
-        <?php
+    <?php
             try {
                 require_once('includes/funciones/bd.php');
-                $sql = " SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+                $sql = " SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento,icono, nombre_invitado, apellido_invitado ";
                 $sql .= " From eventos ";
                 $sql .= " INNER JOIN categoria_evento ";
                 $sql .= " ON eventos.id_categoria_evento = categoria_evento.id_categoria ";
@@ -21,8 +21,8 @@
             }
         ?>
 
-        <div class="calendario">
-            <?php
+    <div class="calendario">
+        <?php
                 $calendario = array();
                 while ( $eventos = $resultado->fetch_assoc()) {
 
@@ -35,6 +35,7 @@
                             'fecha' => $eventos['fecha_evento'],
                             'hora' => $eventos['hora_evento'],
                             'categoria' => $eventos['cat_evento'],
+                            'icono' => 'fas' . " " . $eventos['icono'],
                             'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
                         );
 
@@ -42,14 +43,14 @@
 
                     ?>
 
-                <?php } //Fin del while ?>
+        <?php } //Fin del while ?>
 
-                <!-- Imprime todos los eventos -->
-                <?php
+        <!-- Imprime todos los eventos -->
+        <?php
                     foreach ($calendario as $dia => $lista_eventos) { ?>
-                        <h3>
-                            <i class="fas fa-calendar"></i>
-                            <?php
+        <h3>
+            <i class="fas fa-calendar"></i>
+            <?php
                             // Unix
                             setlocale(LC_TIME, 'es_ES.UTF-8');
                             // Windows
@@ -57,18 +58,36 @@
 
                             // Arreglando mis acentos UTF8_ENCODE()
                             echo utf8_encode(strftime("%A, %d de %B del %Y", strtotime($dia))); ?>
-                        </h3>
-                    <?php } ?>
+        </h3>
+        <?php foreach ($lista_eventos as $evento) { ?>
 
-                <pre>
-                    <?php var_dump($calendario); ?>
-                </pre>
-
-
+        <div class="dia">
+            <p class="titulo">
+                <?php echo $evento['titulo'] ?>
+            </p>
+            <p class="hora"><i class="fas fa-clock"></i>
+                <?php echo $evento['fecha']. " " . $evento['hora'] ?>
+            </p>
+            <p class="categoria">
+                <i class="<?php echo $evento['icono'] ?>"></i>
+                <?php echo $evento['categoria'] ?>
+            </p>
+            <p class="invitado">
+                <i class="fas fa-user"></i>
+                <?php echo $evento['invitado'] ?>
+            </p>
         </div>
 
-        <?php $conn->close();?>
+        <?php } //Fin foreach eventos ?>
+        <?php } //Fin foreach dias?>
 
-    </section>
+    </div>
+    <pre>
+        <?php var_dump($calendario); ?>
+    </pre>
+
+    <?php $conn->close();?>
+
+</section>
 
 <?php include_once 'includes/templates/footer.php'; ?>
--- REMSO/js/main.js
+++ REMSO/js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
 
+    new WOW().init();
+
+    // All animations will take exactly 500ms
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 1000,
+        offset: 87,
+        speedAsDuration: true
+    });
+
+    // OCultar flecha
+    $(function() {
+        $(window).scroll(function() {
+            var scrolltop = $(this).scrollTop();
+            if (scrolltop >= 50) {
+                $(".arriba").fadeIn();
+            } else {
+                $(".arriba").fadeOut();
+            }
+        });
+    });
+
+    // menu animado
+    $(window).scroll(function() {
+        var nav = $('.encabezado');
+        var scroll = $(window).scrollTop();
+        if (scroll >= 80) {
+            nav.addClass("fondo-menu");
+        } else {
+            nav.removeClass("fondo-menu");
+        }
+    });
+
+
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 0,
